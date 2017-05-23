344f4cf1cd58d8c1f3eeca6afcd79e8ae24726ce
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2017-05-23 17:27:39 +0900

    Updated python wordcound script to new API

--- py_wc.py
+++ py_wc.py
@@ -1,17 +1,19 @@
 from veloxmr_lib import *
 
-def map(line):
-	d = { }
+def map(line, output, options):
 	for i in line.split():
-		if i in d:
-			d[i].append('1')
+		if i in output:
+			output[i].append('1')
 		else:
-			d[i] = ['1']
-	return d
+			output[i] = ['1']
 
-def reduce(a,b):
-	return str(int(a)+int(b))
+def reduce(key, values, output):
+	total_size = 0
+	for value in values:
+		total_size += int(value)
+	
+	output[key] = str(total_size);
 
 mapreduce(sys.argv[1], map, reduce, "wc-output")
 cat("wc-output")
-mapreduce("wc-output", map, reduce, "total-output")
+#mapreduce("wc-output", map, reduce, "total-output")
